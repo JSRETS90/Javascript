ae6b4148437efc4f442254a03b14ab3d61e92c51
Jorge Sanchez <144870456+JSRETS90@users.noreply.github.com>
2024-09-18 18:45:51 -0700

    "toggle"

--- proyectos/nav-bar/js/app.js
+++ proyectos/nav-bar/js/app.js
@@ -1,1 +1,17 @@
-alert("orale")
+(function(){
+    const listElements = document.querySelectorAll('.menu_item--show');
+    const list = document.querySelector('.menu_links');
+    const menu = document.querySelector('.menu_hamburger');
+
+    const addClick = ()=> {
+        listElements.forEach(element => {
+            element.addEventListener('click', ()=> {
+               
+                let subMenu = element.children[1];
+                element.classList.toggle('menu_item--active');
+            });
+        });
+    }
+
+    addClick();
+})();
