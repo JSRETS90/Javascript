6545a176a7d5bee31485c46f601dffc71f17c947
Jorge Sanchez <144870456+JSRETS90@users.noreply.github.com>
2024-10-16 19:22:14 -0700

    "finished"

--- slider/slider.js
+++ slider/slider.js
@@ -1,19 +1,26 @@
-(function() {
+(function () {
     const sliders = [...document.querySelectorAll('.slider_body')];
     const arrowNext = document.querySelector('#next');
     const arrowBefore = document.querySelector('#before');
     let value;
 
-    arrowNext.addEventListener('click', ()=>changePosition(1));
-    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+    arrowNext.addEventListener('click', () => changePosition(1));
+    arrowBefore.addEventListener('click', () => changePosition(-1));
 
     function changePosition(change) {
         const currentElement = Number(document.querySelector('.slider_body--show').dataset.id);
 
         value = currentElement;
-        value+= change;
+        value += change;
 
-        console.log (value)
+        if (value === 0 || value === sliders.length + 1) {
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        // Remove the current slider's active class
+        sliders[currentElement - 1].classList.remove('slider_body--show');
+        
+        // Add the active class to the new slider
+        sliders[value - 1].classList.add('slider_body--show');
     }
-    
-})()
+})();
